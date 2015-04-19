8abe66f8457229cb0d6581baa2937ac4159a3cf7
starpan123 <m13110032289@163.com>
2015-04-19 17:45:33 +0800

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-for i in range(1,10):
-    s=""
-    for j in range(i,10):
-        s+=str.format("{0:1}*{1:1}={2:<4}",i,j,i*j)
-    print(s)
-    for n in range(1,1+i):
-        print(end='        ')
- 
